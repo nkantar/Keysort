7aae5e3f43aed068d55869426896f0313c3b1c93
Nik Kantar <nik@nkantar.com>
2018-01-31 22:48:55 -0800

    Add setup.py and CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Keysort Changelog
+
+
+## [0.1.0] - 2018-01-31
+
+### Added
+- Keysort v0.1.0 code
+- Tests...well, a test
+- setup.py
+- Travis CI config
+- README
+- License
+- Code of Conduct
+- Changelog
+- .gitignore
+
+
+[Unreleased]: https://github.com/nkantar/Keysort/compare/0.1.0...HEAD
+[0.1.0]: https://github.com/nkantar/Keysort/releases/tag/0.1.0
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name = 'keysort',
+      packages = ['keysort'],
+      version = '0.1.0',
+      description = 'A small utility for sorting lists of dictionaries by dictionary key',
+      author = 'Nik Kantar',
+      author_email = 'nik@nkantar.com',
+      url = 'https://github.com/nkantar/Keysort',
+      download_url = 'https://github.com/nkantar/Keysort/tarball/0.1.0',
+      keywords = ['dictonary sorting'],
+      classifiers = [],
+      license = 'BSD-3-Clause',
+      zip_safe = False)
