1b26af5a95d914a59054462ca485b9aa6cdb4995
Nik Kantar <nik@nkantar.com>
2023-09-08 15:47:05 -0700

    Add attrsort for objects

---
+++ tests/test_attrsort.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+
+from keysort import attrsort
+
+
+@dataclass
+class MyObj:
+    col1: str
+    col2: str
+    col3: str
+
+
+SORTED_LIST = [
+    MyObj(col1="a", col2="1", col3="A"),
+    MyObj(col1="a", col2="1", col3="B"),
+    MyObj(col1="a", col2="2", col3="A"),
+    MyObj(col1="a", col2="2", col3="B"),
+    MyObj(col1="b", col2="1", col3="A"),
+    MyObj(col1="b", col2="1", col3="B"),
+    MyObj(col1="b", col2="2", col3="A"),
+    MyObj(col1="b", col2="2", col3="B"),
+    MyObj(col1="c", col2="1", col3="A"),
+    MyObj(col1="c", col2="1", col3="B"),
+    MyObj(col1="c", col2="2", col3="A"),
+    MyObj(col1="c", col2="2", col3="B"),
+    MyObj(col1="d", col2="1", col3="A"),
+    MyObj(col1="d", col2="1", col3="B"),
+    MyObj(col1="d", col2="2", col3="A"),
+    MyObj(col1="d", col2="2", col3="B"),
+]
+
+UNSORTED_LIST = [
+    MyObj(col1="a", col2="2", col3="A"),
+    MyObj(col1="d", col2="2", col3="A"),
+    MyObj(col1="b", col2="1", col3="B"),
+    MyObj(col1="a", col2="2", col3="B"),
+    MyObj(col1="b", col2="1", col3="A"),
+    MyObj(col1="b", col2="2", col3="B"),
+    MyObj(col1="d", col2="1", col3="B"),
+    MyObj(col1="a", col2="1", col3="B"),
+    MyObj(col1="c", col2="2", col3="B"),
+    MyObj(col1="c", col2="2", col3="A"),
+    MyObj(col1="c", col2="1", col3="B"),
+    MyObj(col1="c", col2="1", col3="A"),
+    MyObj(col1="b", col2="2", col3="A"),
+    MyObj(col1="a", col2="1", col3="A"),
+    MyObj(col1="d", col2="2", col3="B"),
+    MyObj(col1="d", col2="1", col3="A"),
+]
+
+
+def test_attrsort():
+    assert attrsort(UNSORTED_LIST, ["col1", "col2", "col3"]) == SORTED_LIST
