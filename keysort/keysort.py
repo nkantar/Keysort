418b985fdfa1027c8b375f86cb70b3cbd55caeae
Nik Kantar <nik@nkantar.com>
2023-09-08 14:51:18 -0700

    Fix typing and docstring issues

--- keysort/keysort.py
+++ keysort/keysort.py
@@ -6,15 +6,19 @@ functions the user is meant to utilize.
 """
 
 
-from typing import Any
+from typing import Any, List
 
 
-def keysort(my_list: list[Any], keys: list[Any] = []) -> list[Any]:
+def keysort(my_list: List[Any], keys: List[Any] = []) -> List[Any]:
     """Sort list of dictionaries by values of multiple keys.
 
     Args:
         my_list (list): List of dictionaries to sort.
-        keys (list): List of
+        keys (list): List of keys by which to sort.
+
+    Returns:
+        list: Sorted list of dictionaries.
+
     """
     if keys:
         first_key = keys[0]
